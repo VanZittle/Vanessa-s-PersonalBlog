8b00b7da88ab6c9b8e932119bcd39c4a48b1fc66
VanZittle <vanzittle@gmail.com>
2024-04-29 17:17:13 -0400

    improve funcionality, multiple posts can be stored and printed now

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,46 +1,64 @@
-const username = document.getElementById('user-names');
-const title = document.getElementById('title-text');
-const content = document.getElementById('content-text');
-const saveButton = document.getElementById('save');
-const posts = []
+const formEl = $('#post-form');
+const usernameInputEl = $('#username-input');
+const titleInputEl = $('#title-input');
+const commentInputEl = $('#comment-input');
+const guestBookDisplayEl = $('#posts-display');
 
-function saveLastPost() {
-  // Save related form data as an object
-  const newPost = {
-    username: username.value,
-    title: title.value,
-    content: content.value.trim(),
-  };
-  posts.push(newPost);
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem('posts', JSON.stringify(posts));
-}
+const printGuestData = function (username, title, comment) {
 
-function renderLastPost() {
-  // Use JSON.parse() to convert text to JavaScript object
-  const lastPost = JSON.parse(localStorage.getItem('posts'));
-  console.log(lastPost);
-  // Check if data is returned, if not exit out of the function
-  if (lastPost !== null) {
-    document.getElementById('saved-username').innerHTML = lastPost[lastPost.length - 1].username;
-    document.getElementById('saved-title').innerHTML = lastPost[lastPost.length - 1].title;
-    document.getElementById('saved-content').innerHTML = lastPost[lastPost.length - 1].content;
-  }
-}
-
-saveButton.addEventListener('click', function (event) {
-  event.preventDefault();
-  saveLastPost();
-  renderLastPost();
+  let guestEntries= JSON.parse(localStorage.getItem('guestEntries')) || [];
   
-});
+  // add new data
+  guestEntries.push ({username, title, comment});
+  // save the update in local storage
+  localStorage.setItem('guestEntries',JSON.stringify(guestEntries));
 
-// The init() function fires when the page is loaded
-function init() {
-  // When the init function is executed, the code inside renderLastGrade function will also execute
-  renderLastPost();
-}
-init();
+  const cardColumnEl = $('<div>');
+  cardColumnEl.addClass('col-12 col-md-4');
+
+  const cardEl = $('<div>');
+  // add a class of .custom-card
+  cardEl.addClass('card h-100');
+  cardEl.appendTo(cardColumnEl);
+
+  // add a class of .custom-card-header
+  const cardName = $('<h5>').addClass('card-header custom-card-header').text(username);
+  cardName.appendTo(cardEl);
+
+  const cardBodyEl = $('<div>');
+  cardBodyEl.addClass('card-body');
+  cardBodyEl.appendTo(cardEl);
+
+  const cardTitle = $('<p>').addClass('card-text').text(title);
+  cardTitle.appendTo(cardBodyEl);
+
+  const cardComment = $('<p>').addClass('card-text').text(comment);
+  cardComment.appendTo(cardBodyEl);
+
+  guestBookDisplayEl.append(cardColumnEl);
+};
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  const usernameInput = usernameInputEl.val();
+  const titleInput = titleInputEl.val();
+  const commentInput = commentInputEl.val();
+
+  if (!usernameInput || !titleInput || !commentInput) {
+    console.log('Fill out all inputs in the form, please');
+    return;
+  }
+
+  printGuestData(usernameInput, titleInput, commentInput);
+
+  // reset form
+  usernameInputEl.val('');
+  titleInputEl.val('');
+  commentInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit);
 
 // Start code to switch to black and white mode
 
